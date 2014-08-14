0dd7115c9f013c34cff381ddf9f5937887a4fdc6
Bjoern Rennhak <bjoern@clothesnetwork.com>
2014-08-14 22:48:19 +0100

    - Trivial formatting of interval.rb

--- lib/geohash36/library/interval.rb
+++ lib/geohash36/library/interval.rb
@@ -1,6 +1,11 @@
+#!/usr/bin/env ruby
+
+
+# @fn       class Interval < Array 
+# @brief    Interval class handling intervall calculation
 class Geohash36::Interval < Array
 
-  def initialize(array = [0, 0], options = {})
+  def initialize array = [0, 0], options = {}
     array.try(:compact!)
     validate_array(array)
     array.each{|element| self.push element}
@@ -8,11 +13,11 @@ class Geohash36::Interval < Array
     @opts = defaults.merge options
   end
 
-  def configure(options = {})
+  def configure options = {}
     @opts.merge! options
   end
 
-  def include?(number)
+  def include? number
     for_left_border  = (@opts[:include_left] == true)  ? first  <= number : first < number
     for_right_number = (@opts[:include_right] == true) ? number <= last  : number < last
     for_left_border && for_right_number
@@ -22,7 +27,7 @@ class Geohash36::Interval < Array
     split3.each_with_object([]){|interval, result| result.concat interval.split2}
   end
 
-  def update(array)
+  def update array
     array.try(:compact!)
     validate_array(array)
     self.clear
@@ -56,7 +61,7 @@ class Geohash36::Interval < Array
     result.map{|array| Geohash36::Interval.new array}
   end
 
-  def self.convert_array(array, options = {})
+  def self.convert_array array, options = {}
     intervals = array.map{|interval| Geohash36::Interval.new interval, options }
     intervals.first.configure(include_left: true) unless options[:include_left]
     intervals.last.configure(include_right: true) unless options[:include_right]
@@ -64,8 +69,12 @@ class Geohash36::Interval < Array
   end
 
   private
-    def validate_array(array)
-      raise ArgumentError, "Not valid array for geohash interval" unless array.length == 2
-    end
 
-end
+  def validate_array array
+    raise ArgumentError, "Not valid array for geohash interval" unless array.length == 2
+  end
+
+end # of class Geohash36::Interval
+
+
+# vim:ts=2:tw=100:wm=100:syntax=ruby
