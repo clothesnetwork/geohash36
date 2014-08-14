4dd257bd3e5ddb0b9c4b020c4e0e8341b348edf6
Bjoern Rennhak <bjoern@clothesnetwork.com>
2014-08-14 22:07:24 +0100

    - Removed useless cucumber files which were from legacy code fork

--- features/support/env.rb
+++
@@ -1,7 +0,0 @@
-require 'aruba/cucumber'
-require 'aruba/in_process'
-require 'geohash36/library/hypermedia/runner'
-
-
-Aruba::InProcess.main_class = Geohash36::Hypermedia::Runner
-Aruba.process = Aruba::InProcess
